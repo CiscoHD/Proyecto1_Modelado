c44703a57ae15995cebdd167e630f7a8e071e0bc
CiscoHD <ciscohdz@ciencias.unam.mx>
2026-09-21 19:29:12 -0600

    feat: En el servidor se agregaron los archivos protocolo.c y protocolo.h
    - Se crean enumeraciones para los campos 'type', 'operation', 'status',
      y 'result'.
    - Se crean estructuras que mapean una cadena a su tipo correspondiente
    - Se crean arreglos constantes que guardan los mapeos.
    - Se crea una fabrica de respuestas que recibe una enumeración con el tipo de
      respuesta y una función que determina si una cadena corresponde a un tipo de
      mensaje devolviendo una enumeración en caso de serlo

---
+++ servidor/src/protocolo.c
@@ -0,0 +1,86 @@
+#include "protocolo.h"
+
+const MapeoEntrada TABLA_ENTRADAS[TOTAL_TIPOS_E] = {
+  {"IDENTIFY", IDENTIFY},
+  {"STATUS",STATUS },
+  {"USERS", USERS},
+  {"ROOM_USERS", ROOM_USERS},
+  {"TEXT", TEXT},
+  {"PUBLIC_TEXT", PUBLIC_TEXT},
+  {"ROOM_TEXT", ROOM_TEXT},
+  {"NEW_ROOM", NEW_ROOM},
+  {"INVITE", INVITE},
+  {"JOIN_ROOM", JOIN_ROOM},
+  {"LEAVE_ROOM", LEAVE_ROOM},
+  {"DISCONNECT", DISCONNECT},
+};
+
+const MapeoRespuestas TABLA_RESP[TOTAL_TIPOS_RESP] = {
+  {"IDENTIFY", IDENTIFY_O},
+  {"INVITATION", INVITATION},
+  {"INVITE", INVITE_O},
+  {"NEW_ROOM", NEW_ROOM_O},
+  {"NEW_USER", NEW_USER},
+  {"NEW_STATUS", NEW_STATUS},
+  {"JOIN_ROOM", JOIN_ROOM_O},
+  {"JOINED_ROOM", JOINED_ROOM},
+  {"LEAVE_ROOM", LEAVE_ROOM_O},
+  {"LEFT_ROOM", LEFT_ROOM},
+  {"DISCONNECTED", DISCONNECTED},
+  {"USER_LIST", USER_LIST},
+  {"ROOM_USERS", ROOM_USERS_O},
+  {"ROOM_USERS_LIST", ROOM_USERS_LIST},
+  {"TEXT", TEXT_O},
+  {"TEXT_FROM", TEXT_FROM},
+  {"PUBLIC_TEXT", PUBLIC_TEXT_O},
+  {"PUBLIC_TEXT_FROM", PUBLIC_TEXT_FROM},
+  {"ROOM_TEXT", ROOM_TEXT_O},
+  {"ROOM_TEXT_FROM", ROOM_TEXT_FROM},
+  {"INVALID", INVALID_O},
+  {"RESPONSE", RESPONSE}
+};
+
+const MapeoResultados TABLA_RESUL[TOTAL_TIPOS_RESUL] = {
+  {"SUCCESS", SUCCESS},
+  {"USER_ALREADY_EXISTS", USER_ALREADY_EXISTS},
+  {"NO_SUCH_USER", NO_SUCH_USER},
+  {"ROOM_ALREADY_EXISTS", ROOM_ALREADY_EXISTS},
+  {"NO_SUCH_ROOM", NO_SUCH_ROOM},
+  {"NOT_INVITED", NOT_INVITED},
+  {"NOT_JOINED", NOT_JOINED},
+  {"INVALID", INVALID}
+};
+
+TipoEntrada tipo_mensaje(const char *mensaje) {
+  if(mensaje == NULL)
+    return -1;
+
+  for(int i=0; i < TOTAL_TIPOS_E; i++){
+    if(strcmp(mensaje, TABLA_ENTRADAS[i].cad_tipo) == 0)
+      return TABLA_ENTRADAS[i].tipo_ent;
+  }
+  return -1;
+}
+
+cJSON *fabrica_respuesta(TipoResp tipo, const Campos *c) {
+  if(tipo < 0 || (c == NULL))
+    return NULL;
+  
+  cJSON *json = cJSON_CreateObject();
+  // Campos c = (datos != NULL) ? datos : (Campos){0};
+
+  switch(tipo){
+  case RESPONSE:
+    cJSON_AddStringToObject(json, "type", TABLA_RESP[RESPONSE].cad_tipo);
+    cJSON_AddStringToObject(json, "operation", TABLA_RESP[c->operation].cad_tipo);
+    cJSON_AddStringToObject(json, "result", TABLA_RESUL[c->result].cad_tipo);
+    if(c->extra != NULL) 
+      cJSON_AddStringToObject(json, "extra", c->extra);
+    break;
+  case NEW_USER:
+    break;
+  }
+  return json;
+}
+
+
