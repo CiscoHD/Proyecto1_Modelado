964abd1afa4a8ee6d6bc6319bc9a04765645f675
CiscoHD <ciscohdz@ciencias.unam.mx>
2026-09-25 01:36:14 -0600

    feat: Modificaciones en servidor.c
    - Se crea una estructura auxiliar para enviar los datos que cada hilo
      necesita (socket del cliente y contexto del servidor).
    - Se modificó la lógica de desconexiones. Si el cliente se desconecta
      abruptamente o lo hace de manera limpia se manda a llamar a la
      función manejar_disconnect() que es capaz de determinar si la
      desconexión está pendiente o se realizó.

--- servidor/src/servidor.c
+++ servidor/src/servidor.c
@@ -2,25 +2,29 @@
 #include <glib.h>
 #include "include/socket_util.h"
 #include "include/distribuidor.h"
+#include "estructuras.h"
 
 #define BUFFER_SIZE 1048576
 
 gpointer entrada_hilo(gpointer datos) {
-  int sockfd_h;                          /*Socket file descriptor en el hilo*/
-  int codigo_error = 0;
-  sockfd_h = *(int *)datos;
-  g_free(datos);
+  DatosHilo *d = (DatosHilo *)datos;
 
+  int sockfd_h = d->socket_fd_h;         /*Socket file descriptor en el hilo*/
+  ContextoServidor *cs = d->contexto;
+  g_free(d);
+  
+  int desconectar = 0;
   char buffer[BUFFER_SIZE];
 
   printf("[Hilo: %p] Conexión con cliente %d\n", g_thread_self(), sockfd_h);
 
-  while(codigo_error != 1) {
+  while(desconectar != 1) {
     memset(buffer, 0, sizeof(buffer));
     ssize_t valread = read(sockfd_h, buffer, sizeof(buffer) - 1);
     
     if(valread <= 0) {
       printf("[Hilo: %p] ERROR: El cliente %d se ha desconectado.\n", g_thread_self(), sockfd_h);
+      desconectar = 1;
       break;
     }
 
@@ -28,10 +32,13 @@ gpointer entrada_hilo(gpointer datos) {
     
     cJSON *json = cJSON_Parse(buffer);
     
-    codigo_error = distribuidor_peticiones(sockfd_h, json);
-    
+    desconectar = distribuidor_peticiones(sockfd_h, json, cs);
+
     cJSON_Delete(json);
   }
+
+  manejar_disconnect(sockfd_h, cs);
+  
   close(sockfd_h);
   return NULL;
 }
@@ -54,19 +61,21 @@ int main(int argc, char *argv[]) {
   
   sockfd = iniciar_conex(puerto, 10, 1);
 
+  ContextoServidor *cs = crear_contexto_servidor(); 
+
   while(1) {
     if((sockfd_usr = aceptar_conex(sockfd, 1)) < 0)
       continue;
 
-    /*Por cada cliente que se conecte, vamos a guardar un apuntador a
-      sockfd_usr para poder pasarlos a g_thread_new()_*/
-    int *psockfd_usr = g_malloc(sizeof(int));
-    *psockfd_usr = sockfd_usr;
+    DatosHilo *datos = (DatosHilo *) malloc(sizeof(DatosHilo));
+    datos->socket_fd_h = sockfd_usr;
+    datos->contexto = cs;
 
     /*Se crea un hilo para atender (con la función entrada_hilo) al
       nuevo cliente*/
-    GThread *hilo = g_thread_new("usuario", entrada_hilo, psockfd_usr);
-    
+    GThread *hilo = g_thread_new("usuario", entrada_hilo, datos);
+
+        
     g_thread_unref(hilo);
   }
   
